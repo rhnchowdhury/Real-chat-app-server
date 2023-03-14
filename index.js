0a209f5f3739a4f34e1ad11afe63d531fb7c64fd
Raihan <rhnchdri@gmail.com>
2023-03-14 12:37:31 +0600

    mongo setup

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ajoxj5t.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const fitnessCollection = client.db('realChat').collection('users');
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
 
 app.get('/', async (req, res) => {
     res.send('Real time app is running');
